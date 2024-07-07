b6eb97877f20f6775213bf5859014a3bb8b72ade
Argisht44 <mamreyan@gmail.com>
2024-07-08 01:25:34 +0300

    Обновление

--- src/App.tsx
+++ src/App.tsx
@@ -53,6 +53,7 @@ const App = () => {
       {/* Фоновое видео */}
       <video className="video-background" autoPlay loop muted playsInline>
       <source src="/src/video/video.mp4" type="video/mp4" />
+      <source src="\src\video\video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       
